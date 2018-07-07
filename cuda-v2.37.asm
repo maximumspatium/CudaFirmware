32d4ab3969f6eb073817dde7630f74a45d79186c
Maxim Poliakovski <maximumspatium@googlemail.com>
2018-07-07 21:45:20 +0200

    Various delay subroutines.

--- cuda-v2.37.asm
+++ cuda-v2.37.asm
@@ -1,41 +1,41 @@
 ;-------------------------------------------------------------------------
 ; Start of Cuda ROM
 ;-------------------------------------------------------------------------
-		org $F00
+        org $F00
         fcc "Copyright © 1989-93"
-		fcb $D
+        fcb $D
         fcc "Some Company Name XX"
-		fcb $D
-		fcc "All rights reserved."
-		fcb   0
-		fcb   0
-		fcb $19
-		fcb   0
-		fcb   2
-		fcb   0
-		fcb $25
-		fcb   1
-		fcb   0
-		fcb   0
-		fcb $A4
-		fcb   0
-		fcb $95
-		fcb   0
-		fcb $AF
-		fcb   0
-		fcb $AB
+        fcb $D
+        fcc "All rights reserved."
+        fcb   0
+        fcb   0
+        fcb $19
+        fcb   0
+        fcb   2
+        fcb   0
+        fcb $25
+        fcb   1
+        fcb   0
+        fcb   0
+        fcb $A4
+        fcb   0
+        fcb $95
+        fcb   0
+        fcb $AF
+        fcb   0
+        fcb $AB
 
-		fcc "Cuda 2.37" ; Human-readable firmware version string
+        fcc "Cuda 2.37" ; Human-readable firmware version string
 
 IOInit:
         org $F57
         fcb 0 ; initial value for PORTA
-		fcb 0 ; initial value for PORTB
-		fcb 0 ; initial value for PORTC
-		fcb 0 ; unused
-		fcb $99   ; initial data direction for Port A (DDRA): OIIOOIIO (O - out, I - in)
-		fcb $92   ; initial data direction for Port B (DDRB): OIIOIIOI
-		fcb 8     ; initial data direction for Port C (DDRC): OIII
+        fcb 0 ; initial value for PORTB
+        fcb 0 ; initial value for PORTC
+        fcb 0 ; unused
+        fcb $99   ; initial data direction for Port A (DDRA): OIIOOIIO (O - out, I - in)
+        fcb $92   ; initial data direction for Port B (DDRB): OIIOIIOI
+        fcb 8     ; initial data direction for Port C (DDRC): OIII
 
 ;--------------------------------------------------------------------------
 ; This entry point is actually the cold start, i.e. it will be called
@@ -51,43 +51,43 @@ IOInit:
 ;--------------------------------------------------------------------------
 
 ColdStart:
-		rsp           ; reset stack pointer (SP <= $FF)
-		jsr	sub_11B3  ; PLL test?
-		lda	#$10
-		sta	CPICSR
-		lda	#3
-		sta	TCS
+        rsp              ; reset stack pointer (SP <= $FF)
+        jsr    sub_11B3  ; PLL test?
+        lda    #$10
+        sta    CPICSR
+        lda    #3
+        sta    TCS
 
         ;------------------------------------------------------------------
         ; write initial values to PORTA, PORTB, PORTC and DDRA, DDRB, DDRC
         ;------------------------------------------------------------------
-		ldx	#6
+        ldx    #6
 loop1:
-		lda	IOInit, x
-		sta	, x
-		decx
-		bpl	loop1
+        lda    IOInit, x
+        sta    , x
+        decx
+        bpl    loop1
 
         ;------------------------------------------------------------------
         ; clear 64 bytes of RAM in the address range $90...$CF
         ; This memory is used to maintain Cuda state
         ;------------------------------------------------------------------
-		ldx	#$40
+        ldx    #$40
 loop2:
-		clr	byte_90, x
-		decx
-		bpl	loop2
+        clr    byte_90, x
+        decx
+        bpl    loop2
 
         ;------------------------------------------------------------------
         ; zero the PRAM area which is actually a part of the MCU's internal
         ; RAM in the address range $100...$1FF.
         ;------------------------------------------------------------------
-		clrx  ; X = 0, loop counter
-		txa   ; A = 0, value to write
+        clrx  ; X = 0, loop counter
+        txa   ; A = 0, value to write
 loop3:
-		sta	word_100, x
-		incx
-		bne	loop3
+        sta    word_100, x
+        incx
+        bne    loop3
 
         ;------------------------------------------------------------------
         ; This is the place where some funny stuff happens :))
@@ -98,27 +98,119 @@ loop3:
         ; that corresponds to Monday, 27. August 1956. This is apparently
         ; the birthdate of Cuda's designer Ray Montagne :)))
         ;------------------------------------------------------------------
-		lda	#$63
-		sta	byte_AB
-		lda	#$B
-		sta	byte_AC
-		lda	#$D1
-		sta	byte_AD
-		lda	#$78
-		sta	byte_AE
+        lda    #$63
+        sta    byte_AB
+        lda    #$B
+        sta    byte_AC
+        lda    #$D1
+        sta    byte_AD
+        lda    #$78
+        sta    byte_AE
 
-		bset	3, byte_A3
-		bset	3, byte_A1
-		jsr	sub_173C
-		sta	byte_91
+        bset    3, byte_A3
+        bset    3, byte_A1
+        jsr    sub_173C
+        sta    byte_91
 
         ;------------------------------------------------------------------
         ; This seems to be the Warm start entry.
         ;------------------------------------------------------------------
 loc_F9B:
-		sei           ; disable interrupts
-		rsp           ; reset stack pointer (SP <= $FF)
-		jsr	sub_11B3  ; PLL init?
+        sei           ; disable interrupts
+        rsp           ; reset stack pointer (SP <= $FF)
+        jsr sub_11B3  ; PLL init?
+
+        org $1E18
+
+;------------------------------------------------------------------
+; Generic delay subroutine that spends 10 * X cycles.
+; To get the real number of cycles spent, add additional 14 cycles
+; for loading the counter + call + ret.
+;------------------------------------------------------------------
+TenCyclesLoop:
+        nop
+        nop
+        decx
+        bne    TenCyclesLoop
+        rts
+
+;---------------------------------------------------------------
+; Another delay routine.
+;---------------------------------------------------------------
+sub_1E1E:
+        lda    #$10
+
+loc_1E20:
+        ldx    #$13
+        jsr    TenCyclesLoop
+        deca
+        bne    loc_1E20
+        rts
+
+;----------------------------------------------------------------
+; This subroutine will wait for at least the specified number of
+; milliseconds.
+; The real delay is a bit longer - we need to add the additional
+; 14 cycles for loading the counter + call + ret.
+; This subroutine works only in the high-speed clock (2.097 MHz)
+; mode.
+; Params: X - number of milliseconds to wait
+;----------------------------------------------------------------
+DelayXMillisecs:
+        cpx    #0
+        beq    nullsub_1
+
+loc_1E2D:
+        lda    #$AE
+
+loc_1E2F:
+        nop
+        nop
+        nop
+        deca
+        bne    loc_1E2F
+        decx
+        bne    loc_1E2D
+
+doNothing:
+        rts
+
+;----------------------------------------------------------------
+; This subroutine will wait for at least 10 milliseconds.
+; It supports both the low-speed (16.384 kHz) and
+; the high-speed (2.097 MHz) mode.
+; The real delay is a bit longer - we need to add the additional
+; 14 cycles for loading the counter + call + ret.
+; Params: X - number of milliseconds to wait
+;----------------------------------------------------------------
+Delay10Millisecs:
+        brset  BCS, PLLC, loc_1E42 ; go if we're running at the high speed
+        ldx    #$F              ; otherwise, wait for 15 * 10 + 14 cycles, i.e.
+        jsr    TenCyclesLoop    ; 1/16384 * 164 * 1000 = 10 ms
+        rts                     ; and return
+
+loc_1E42:
+        ldx    #$A              ; wait for 10 ms in the high-speed
+        jsr    DelayXMillisecs  ; mode (2.097 MHz)
+        rts                     ; and return
+
+;----------------------------------------------------------------
+; This subroutine will wait for at least 10 milliseconds.
+; The real delay is a bit longer - we need to add the additional
+; 14 cycles for loading the counter + call + ret.
+; Params: X - number of milliseconds to wait
+;----------------------------------------------------------------
+Delay100Millisecs:
+        brset  BCS, PLLC, loc_1E51 ; go if we're running at the high speed
+        ldx    #$A1             ; otherwise, wait for 161 * 10 + 14 cycles, i.e.
+        jsr    TenCyclesLoop    ; 1/16384 * 1624 * 1000 = 100 ms
+        rts                     ; and return
+
+loc_1E51:
+        ldx    #$64             ; wait for 100 ms in the high-speed
+        jsr    DelayXMillisecs  ; mode (2.097 MHz)
+        rts                     ; and return
+
 
 ;---------------------------------------------------------------
 ; Self-check ROM ($1F00...$1FEF).
@@ -130,15 +222,15 @@ loc_F9B:
 ;---------------------------------------------------------------
 ; Interrupt vectors
 ;---------------------------------------------------------------
-		org $1FF0
-        fcb   0			 ; reserved
-        fcb   0			 ; reserved
-        fcb   0			 ; reserved
-        fcb   0			 ; reserved
-        fcb   0			 ; reserved
-        fcb   0			 ; reserved
-        fdb CPIHandler	 ; Custom Periodic Interrupt
-    	fdb TIHandler    ; Timer Interrupt
+        org $1FF0
+        fcb   0             ; reserved
+        fcb   0             ; reserved
+        fcb   0             ; reserved
+        fcb   0             ; reserved
+        fcb   0             ; reserved
+        fcb   0             ; reserved
+        fdb CPIHandler     ; Custom Periodic Interrupt
+        fdb TIHandler    ; Timer Interrupt
         fdb IRQHandler   ; IRQ/IRQ2
-        fdb 0		     ; SWI (unused)
-        fdb ColdStart	 ; Cuda HW Reset
+        fdb 0             ; SWI (unused)
+        fdb ColdStart     ; Cuda HW Reset
